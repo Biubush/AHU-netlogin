4247231497e320880a132c938c5424f0563c646c
Era <1577141450@qq.com>
2022-05-07 01:33:23 +0800

    完善脚本

--- AHU-netlogin.sh
+++ AHU-netlogin.sh
@@ -1,3 +1,7 @@
+wget https://github.com/Biubush/AHU-netlogin/releases/download/AHU-netlogin/AHU-netlogin.py -O /etc/AHU-netlogin.py
+chmod 777 /etc/AHU-netlogin.py
+wget https://github.com/Biubush/AHU-netlogin/releases/download/AHU-netlogin/AHU-netlogin.service -O /etc/AHU-netlogin.service
+chmod 777 /etc/AHU-netlogin.service
 systemctl daemon-reload
 systemctl enable AHU-netlogin
 systemctl start AHU-netlogin
